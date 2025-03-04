3d377e8ac40900c7ff514a25347a89fd35a9e5a5
martinezmauri <maurimartine01@gmail.com>
2025-03-04 20:51:54 -0300

    Agrego seeder Genre y modifico app

--- src/app.js
+++ src/app.js
@@ -1,31 +1,20 @@
 import express from "express";
 import router from "./routes/indexRoutes.js";
 import GlobalMiddleware from "./middlewares/GlobalMiddleware.js";
-import FilmRepository from "./repository/FilmRepository.js";
-import { readFile } from "fs/promises";
+import seedDatabase from "./helpers/seeder.js";
 import cors from "cors";
-
-const loadMovies = async () => {
-  const data = await readFile(
-    new URL("./helpers/movies.json", import.meta.url)
-  );
-  return JSON.parse(data);
-};
 
 class App {
   constructor() {
     this.app = express();
     this.middlewares();
     this.routes();
-    this.seedDatabase();
+    this.loadInitialData();
   }
 
-  async seedDatabase() {
+  async loadInitialData() {
     try {
-      const movies = await loadMovies();
-      const filmRepo = new FilmRepository();
-      filmRepo.seederFilm(movies);
-      console.log("Películas cargadas en la base de datos.");
+      await seedDatabase();
     } catch (error) {
       console.error("Error al cargar las películas:", error);
     }
